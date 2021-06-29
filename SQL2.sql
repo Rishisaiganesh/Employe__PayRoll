88bafa0906c9e0977bcc8dcae5eb7ee361d6dc1b
Rishisaiganesh <crazyganesh67@gmail.com>
2021-06-29 20:21:42 +0530

    [Rishi]Selection from Table

---
+++ SQL2.sql
@@ -0,0 +1,21 @@
+//UC1
+create database EmployePay_
+
+USE EmployePay_;
+//Uc2
+create table Employess(
+EmployeID int identity(1,1) primary key,
+EmployeName varchar(200) not null,
+Salary float not null,
+StartingDate date not null);
+//UC3
+Insert into Employess(EmployeID,EmployeName,Salary,StartingDate)
+values (222222,'Rishi',55500.2,'20-02-2021');
+
+//UC4
+select * from Employess
+//UC5
+select EmployeName,Salary from Employess Where EmployeName = 'Rishi';
+
+
+
